2badeb77ee9d9b83d79c2328b0a569627ecd91d6
Damazoisai <118004204+Damazoisai@users.noreply.github.com>
2022-11-24 02:09:33 -0500

    Update Archivo.py

--- Archivo.py
+++ Archivo.py
@@ -2,5 +2,13 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import urllib.request
+
+@st.experimental_menu
+def download_data():
+  url= 'https://www.datosabiertos.gob.pe/dataset/sunedu-licenciamiento-institucional/resource/b582532d-3202-4de3-a7ea-b35bd97f7079'
+  filename='data.csv'
+  urllib.request.urlretrieve(url,filename)
+download_data()
 
 
