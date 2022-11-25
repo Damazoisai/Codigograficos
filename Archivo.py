2d854ff1149a95494fe68af6b8262bea423d5058
Damazoisai <118004204+Damazoisai@users.noreply.github.com>
2022-11-25 09:36:54 -0500

    Update Archivo.py

--- Archivo.py
+++ Archivo.py
@@ -4,18 +4,17 @@ import pandas as pd
 import numpy as np
 import urllib.request
 from PIL import Image
-import datetime
-import matplotlib.pyplot as plt 
+
     
 @st.experimental_memo
-def df():
+def download_data():
     url ="https://raw.githubusercontent.com/Damazoisai/Codigograficos/main/Archivo%20para%20subir%20al%20github.csv"
     filename ="Archivo%20para%20subir%20al%20github.csv"
     urllib.request.urlretrieve(url,filename)
     Licenciamiento = pd.read_csv('Archivo%20para%20subir%20al%20github.csv')
     return Licenciamiento
-df()
-#st.dataframe(download_data())        lee la tabla
+download_data()
+st.dataframe(download_data())        #lee la tabla
 #para leer imagen 
 image=Image.open("messi.jpeg")
 st.image(image)
