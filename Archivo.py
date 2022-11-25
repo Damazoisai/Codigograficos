aa3d1fd8e4f2ce6118d122c3a5b9adcc2a66309a
Damazoisai <118004204+Damazoisai@users.noreply.github.com>
2022-11-25 08:43:49 -0500

    Update Archivo.py

--- Archivo.py
+++ Archivo.py
@@ -6,10 +6,10 @@ import urllib.request
     
 @st.experimental_memo
 def download_data():
-    url ="https://github.com/Damazoisai/Codigograficos/blob/main/Licenciamiento%20Institucional.xlsx"
-    filename ="Licenciamiento%20Institucional.xlsx"
+    url ="https://raw.githubusercontent.com/Damazoisai/Codigograficos/main/Archivo%20para%20subir%20al%20github.csv"
+    filename ="Archivo%20para%20subir%20al%20github.csv"
     urllib.request.urlretrieve(url,filename)
-    Licenciamiento = pd.read_xlxs('Licenciamiento%20Institucional.xlsx')
+    Licenciamiento = pd.read_csv('Archivo%20para%20subir%20al%20github.csv')
     return Licenciamiento
 download_data()
    
