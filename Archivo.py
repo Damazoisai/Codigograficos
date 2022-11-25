7b9a388711139f4e9569ba48d676b92b65240a8a
Damazoisai <118004204+Damazoisai@users.noreply.github.com>
2022-11-25 09:26:33 -0500

    Update Archivo.py

--- Archivo.py
+++ Archivo.py
@@ -4,15 +4,17 @@ import pandas as pd
 import numpy as np
 import urllib.request
 from PIL import Image
+import datetime
+import matplotlib.pyplot as plt 
     
 @st.experimental_memo
-def download_data():
+def df():
     url ="https://raw.githubusercontent.com/Damazoisai/Codigograficos/main/Archivo%20para%20subir%20al%20github.csv"
     filename ="Archivo%20para%20subir%20al%20github.csv"
     urllib.request.urlretrieve(url,filename)
     Licenciamiento = pd.read_csv('Archivo%20para%20subir%20al%20github.csv')
     return Licenciamiento
-download_data()
+df()
 #st.dataframe(download_data())        lee la tabla
 #para leer imagen 
 image=Image.open("messi.jpeg")
@@ -20,6 +22,19 @@ st.image(image)
 
    
 #codigo de graficos 
-chart_data = pd.DataFrame(np.random.randn(20, 3),columns=["a", "b", "c"])
-st.bar_chart(chart_data)
-st.write("Mañana juega argentina")
+#chart_data = pd.DataFrame(np.random.randn(20, 3),columns=["a", "b", "c"])
+#st.bar_chart(chart_data)
+
+#Graficos de Richerd
+
+fechas = df["FECHA_INICIO_LICENCIAMIENTO"]
+periodo = df["PERIODO_LICENCIAMIENTO"]
+x=[datetime.datetime.strptime(str(date), '%Y-%m-%d %H:%M:%S').date() for date in fechas]
+y=periodo
+plt.barh(x,y)
+plt.gcf().set_size_inches(15, 5)
+plt.title("FECHA_INICIO_LICENCIAMIENTO vs PERIODO_LICENCIAMIENTO")
+plt.xlabel("FECHA_INICIO_LICENCIAMIENTO")
+plt.ylabel("PERIODO_LICENCIAMIENTO")
+plt.grid(axis = 'x')
+plt.show()
