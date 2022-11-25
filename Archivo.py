5314e91836f9eb0888edc3d2ee60e73f23aa1298
Damazoisai <118004204+Damazoisai@users.noreply.github.com>
2022-11-25 09:33:00 -0500

    Update Archivo.py

--- Archivo.py
+++ Archivo.py
@@ -25,16 +25,5 @@ st.image(image)
 #chart_data = pd.DataFrame(np.random.randn(20, 3),columns=["a", "b", "c"])
 #st.bar_chart(chart_data)
 
-#Graficos de Richerd
 
-fechas = df["FECHA_INICIO_LICENCIAMIENTO"]
-periodo = df["PERIODO_LICENCIAMIENTO"]
-x=[datetime.datetime.strptime(str(date), '%Y-%m-%d %H:%M:%S').date() for date in fechas]
-y=periodo
-plt.barh(x,y)
-plt.gcf().set_size_inches(15, 5)
-plt.title("FECHA_INICIO_LICENCIAMIENTO vs PERIODO_LICENCIAMIENTO")
-plt.xlabel("FECHA_INICIO_LICENCIAMIENTO")
-plt.ylabel("PERIODO_LICENCIAMIENTO")
-plt.grid(axis = 'x')
-plt.show()
+
