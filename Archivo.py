c8f3d71c09b7ffd1eadfa0ab7775eaa418c543f8
Damazoisai <118004204+Damazoisai@users.noreply.github.com>
2022-11-25 02:00:42 -0500

    Update Archivo.py

--- Archivo.py
+++ Archivo.py
@@ -1,28 +1,73 @@
 #$ pip install streamlit --upgrade
 import streamlit as st
-import urllib.request
 import pandas as pd
 import numpy as np
-st.header("Licenciamiento institucional")
-@st.experimental_memo
+from streamlit_option_menu import option_menu
+import urllib.request
+import matplotlib.pyplot as plt
+import plotly.express as px
+import folium
+from streamlit_folium import st_folium
+from PIL import Image
 
-def download_data():
-   url="https://www.datosabiertos.gob.pe/sites/default/files/Licenciamiento%20Institucional_7.csv"
-   filename="Licenciamiento_institucional.xlsx"
-   urllib.request.urlretrieve(url,filename)
-   df=pd.read_csv('Licenciamiento_institucional.xlsx')
-   return df
-c=download_data()
-st.write('Dimensiones: ' + str(c.shape[0]) + ' filas y ' + str(c.shape[1]) + ' columnas')
-st.dataframe(c)
-st.subheader("Características del Dataset")
-st.write(c.describe())
+#URL del archivo en formato raw
+#url ='https://raw.githubusercontent.com/ximenarojo/prueba/main/Licenciamiento%20Institucional_2.csv'
+#Descargar y leer el archivo y considerar las comas como separadores
+#datos = pd.read_csv(url, sep=',')
+#st.line_chart(data=datos, x='NOMBRE', y='ESTADO_LICENCIAMIENTO')
 
-#st.title('Contaminantes') 
-
-#url del archivo en formato raw
-#url = 'https://raw.githubusercontent.com/brigytt/G_PROGRA/main/Catalogo1960_2021.csv'
-#datos = pd.read_csv(url,sep= ',')
-#st.line_chart(data=datos, x='FECHA_UTC', y='MAGNITUD')
-
-
+#---------------------------------------------------------
+st.set_page_config(layout='wide', initial_sidebar_state='expanded')
+with open('upch.css') as f:
+    st.markdown(f'<style>{f.read()}</upch>', unsafe_allow_html=True)
+with st.sidebar: 
+    st.markdown("###")
+    st.sidebar.header('Programación Avanzada - Proyecto Final 2022-2')
+    st.sidebar.info('Análisis y exploración de datos sobre el avance y estatus del Licenciamiento Institucional de las universidades peruanas.')
+    selected = option_menu(
+        menu_title = 'Menú',
+        options = ['Inicio', 'Localización','Reportes','Equipo'],
+        icons = ['house', 'map', 'book','people'],
+        menu_icon='cast',
+        default_index = 0,
+        styles={
+            "nav-link-selected":{"background-color":"skyblue"}
+        },
+    )
+#--------------------------------------------------------- 
+if selected == 'Inicio':
+    st.markdown("<h1 style ='text-align: center'>SUNEDU:</h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style ='text-align: center'>Licenciamiento Institucional</h1>", unsafe_allow_html=True)
+    st.markdown("---")
+    st.subheader('Contexto:')
+    st.write("En la actualidad, el principal reto que enfrenta la sociedad peruana en términos de educación superior es el de reorganizar el sistema universitario y promover uno basado en la calidad. Es así que en el 2014, la promulgación de la Ley Universitaria, Ley Nº 30220, introduce el licenciamiento obligatorio y renovable para las universidades tanto públicas como privadas del país con la finalidad de asegurar que se brinde un servicio educativo superior que cumpla con las Condiciones Básicas de Calidad (CBC) establecidas.")
+    image = Image.open('Sunedu.jpg')
+    st.image(image) 
+    st.write("**Fuente**: Andina, 2021.")
+    st.subheader('¿Qué es el Licenciamiento Institucional?')
+    st.write("El Licenciamiento Institucional es un procedimiento obligatorio cuyo objetivo es verificar que las universidades cumplan con las CBC a fin de obtener una licencia que autorice su funcionamiento legal y, de esta manera, poder ofrecer un servicio educativo superior universitario. Como resultado, existe un sistema universitario más ordenado y con una mayor orientación hacia la mejora continua.")
+    st.subheader('¿Qué son las Condiciones Básicas de Calidad?')
+    st.write('Las Condiciones Básicas de Calidad (CBC) son un conjunto de estándares mínimos que constituyen un mecanismo de protección a los estudiantes, sus familias y la sociedad en conjunto, con los que una universidad debe contar para obtener el licenciamiento.')
+    image = Image.open('CBC.jpeg')
+    st.image(image)
+    st.write("**Fuente**: SUNEDU, 2018.")
+    st.subheader('Etapas del Licenciamiento Institucional:')
+    video_file = open('Etapas del Licenciamiento para las universidades.mp4', 'rb')
+    video_bytes = video_file.read()
+    st.video(video_bytes)
+    st.write('**Fuente**: SUNEDU, 2015.')
+    st.markdown("---")
+    st.header("Estatus del Licenciamiento Institucional:")
+    st.caption('La información presentada a continuación permite acceder al Dataset “Licenciamiento Institucional” elaborado por la Superintendencia Nacional de Educación Superior Universitaria (SUNEDU) donde se ha registrado el avance y estatus del Licenciamiento Institucional de las universidades del Perú.')
+    st.caption ('Última actualización: 31/08/2022.')
+    
+    @st.experimental_memo
+    def download_data():
+        url ="https://raw.githubusercontent.com/ximenarojo/prueba/main/Licenciamiento%20Institucional_2.csv"
+        filename ="Licenciamiento%20Institucional_2.csv"
+        urllib.request.urlretrieve(url,filename)
+        df_LI = pd.read_csv('Licenciamiento%20Institucional_2.csv')
+        return df_LI
+    download_data()
+    st.dataframe(download_data())
+    st.caption('Para mayor información acceder a: https://www.datosabiertos.gob.pe/dataset/sunedu-licenciamiento-institucional')
