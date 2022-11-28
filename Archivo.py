c9865584f7045f13f9f60926e6462db6c64e993c
Damazoisai <118004204+Damazoisai@users.noreply.github.com>
2022-11-28 16:55:27 -0500

    Update Archivo.py

--- Archivo.py
+++ Archivo.py
@@ -26,6 +26,16 @@ Licenciamiento = pd.read_csv('Archivo%20para%20subir%20al%20github.csv')
 df_anho_freq = pd.DataFrame(Licenciamiento["DEPARTAMENTO"].value_counts())
 st.bar_chart(df_anho_freq)
 #Mostrar datos de la licenciatura 
+# Add annotations
+ANNOTATIONS = [
+    ("Mar 01, 2008", "Pretty good day for GOOG"),
+    ("Dec 01, 2007", "Something's going wrong for GOOG & AAPL"),
+    ("Nov 01, 2008", "Market starts again thanks to..."),
+    ("Dec 01, 2009", "Small crash for GOOG after..."),
+]
+annotations_df = pd.DataFrame(ANNOTATIONS, columns=["date", "event"])
+annotations_df.date = pd.to_datetime(annotations_df.date)
+annotations_df["y"] = 10
 
 
 
