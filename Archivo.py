afb8e8446b7c247e42a30dd38b4b249eb2df5016
Damazoisai <118004204+Damazoisai@users.noreply.github.com>
2022-11-28 15:17:59 -0500

    Update Archivo.py

--- Archivo.py
+++ Archivo.py
@@ -20,7 +20,7 @@ image=Image.open("messi.jpeg")
 st.image(image)
 
 
-   
+st.markdown("Cantidad de universidades en el peru")   
 #codigo de graficos 
 Licenciamiento = pd.read_csv('Archivo%20para%20subir%20al%20github.csv')
 df_anho_freq = pd.DataFrame(Licenciamiento["DEPARTAMENTO"].value_counts())
