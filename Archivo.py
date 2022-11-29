54910ad468ae9cfdb9d7e80119cb7b25ebeec15a
Damazoisai <118004204+Damazoisai@users.noreply.github.com>
2022-11-28 20:03:10 -0500

    Update Archivo.py

--- Archivo.py
+++ Archivo.py
@@ -15,9 +15,6 @@ def download_data():
     return Licenciamiento
 download_data()
 st.dataframe(download_data())        #lee la tabla
-#para leer imagen 
-image=Image.open("messi.jpeg")
-st.image(image)
 
 
 st.markdown("Cantidad de universidades en el peru")   
@@ -25,17 +22,6 @@ st.markdown("Cantidad de universidades en el peru")
 Licenciamiento = pd.read_csv('Archivo%20para%20subir%20al%20github.csv')
 df_anho_freq = pd.DataFrame(Licenciamiento["DEPARTAMENTO"].value_counts())
 st.bar_chart(df_anho_freq)
-#Mostrar datos de la licenciatura 
-# Add annotations
-df_anho_freq = [
-    ("Mar 01, 2008", "Pretty good day for GOOG"),
-    ("Dec 01, 2007", "Something's going wrong for GOOG & AAPL"),
-    ("Nov 01, 2008", "Market starts again thanks to..."),
-    ("Dec 01, 2009", "Small crash for GOOG after..."),
-]
-annotations_df = pd.DataFrame(Licenciamiento, columns=["NOMBRE", "TIPO_GESTION"])
-annotations_df.date = pd.licenciamiento(annotations_df.date)
-annotations_df["y"] = 10
 
 
 
