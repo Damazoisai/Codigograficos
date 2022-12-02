714bfe945a4b0d5a20becd4203a119072bd59b28
Damazoisai <118004204+Damazoisai@users.noreply.github.com>
2022-12-02 09:19:11 -0500

    Update Archivo.py

--- Archivo.py
+++ Archivo.py
@@ -25,9 +25,9 @@ Licenciamiento = pd.read_csv('Archivo%20para%20subir%20al%20github.csv')
 df_anho_freq = pd.DataFrame(Licenciamiento["DEPARTAMENTO"].value_counts())
 st.bar_chart(df_anho_freq)
 #Grafico con fechas 
-start_time = st.slider("Ver universidades segun fecha",value=datetime(2010, 1, 1, 9, 30),format="DD/MM/YY - hh:mm")
+start_time = st.slider("Ver universidades segun fecha inicial",value=datetime(2010, 1, 1, 9, 30),format="DD/MM/YY - hh:mm")
 st.write("Fecha seleccionada:", start_time)
-start_time = st.slider(value=datetime(2010, 1, 1, 9, 30),format="DD/MM/YY - hh:mm")
+start_time = st.slider("Ver universidades segun fecha final"value=datetime(2010, 1, 1, 9, 30),format="DD/MM/YY - hh:mm")
 st.write("Fecha seleccionada:", start_time)
 
 
